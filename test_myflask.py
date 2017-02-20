15c6928f363d71a7576cac52c0ba5ee4c5abcc1d
Cloud User <centos@ip-10-0-2-157.(none)>
2017-02-20 15:30:55 +0000

    Flask webserver

---
+++ test_myflask.py
@@ -0,0 +1,14 @@
+import unittest
+import requests
+
+class myappTestCase(unittest.TestCase):
+  """Test access to myapp Flask"""
+
+  def test_root_page(self):
+    resp = requests.get('http://127.0.0.1:5000/')
+    root_text = resp.text
+    self.assertEqual(root_text, 'Hello iWorld')
+
+unittest.main() 
+
+
