05a0e67197c33168cacbff75a887c5e155613354
howard <centos@ip-10-0-2-157.us-west-2.compute.internal>
2017-02-21 16:39:06 +0000

    test now used private ip

--- test_myflask.py
+++ test_myflask.py
@@ -5,7 +5,7 @@ class myappTestCase(unittest.TestCase):
   """Test access to myapp which is written in Flask and python"""
 
   def test_root_page(self):
-    resp = requests.get('http://54.202.80.197:5000/')
+    resp = requests.get('http://10.0.2.182:5000/')
     root_text = resp.text
     self.assertEqual(root_text, 'Hello world')
 
